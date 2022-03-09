100fd9ff717f8210806504d31c94faa89a2afc87
unknown <khanhbi1998pt@gmail.com>
2022-03-09 16:57:51 +0700

    update

--- src/controller/EmployeeManager.java
+++ src/controller/EmployeeManager.java
@@ -31,15 +31,51 @@ public class EmployeeManager {
         }
     }
 
-    public static int getEmployeeById(String employeeId) {
+//    public static int getEmployeeById(String employeeId) {
+//        for (int i = 0; i < employeeList.size(); i++) {
+//            Employee employee = employeeList.get(i);
+//            if (employee.getEmployeeID().equals(employeeId)) {
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
+
+    public static void removeEmployeeByIndex (int index) {
+        employeeList.remove(index);
+        try {
+            emloyeeData.writeFile(employeeList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int findEmpoyeeIndexById(String employeeId) {
+        int index = -1;
         for (int i = 0; i < employeeList.size(); i++) {
-            Employee employee = employeeList.get(i);
-            if (employee.getEmployeeID().equals(employeeId)) {
-                return i;
+            Employee currentEmployee = employeeList.get(i);
+            String currentEmployeeId = currentEmployee.getEmployeeID();
+            if (currentEmployeeId.equals(employeeId)) {
+                index = i;
+                return index;
             }
         }
-        return -1;
+        return index;
     }
+
+    public static int findEmployeeIndexByName(String searchName) {
+        int index = -1;
+        for (int i = 0; i < employeeList.size(); i++) {
+            Employee currentEmployee = employeeList.get(i);
+            String currentEmployeeName = currentEmployee.getFullName();
+            if (currentEmployeeName.equals(searchName)) {
+                index = i;
+                return index;
+            }
+        }
+        return index;
+    }
+
 
     public static void editEmployeeByIndex(int index, Employee employee) {
         employeeList.set(index, employee);
--- src/view/Client.java
+++ src/view/Client.java
@@ -28,13 +28,14 @@ public class Client {
             choice = inputChoice.nextInt();
             switch (choice){
                 case 1:
-                    addNewEmployee();
+                    Employee newEmployee = creatNewEmployee();
+                    EmployeeManager.addNewEmployee(newEmployee);
                     break;
                 case 2:
                     showAllEmployee();
                     break;
                 case 3:
-
+                   editEmployee();
                     break;
                 case 4:
                      deleteEmployee();
@@ -53,47 +54,92 @@ public class Client {
         }
     }
 
-    public static void deleteEmployee() {
-        Scanner inputID = new Scanner(System.in);
-        System.out.println("Nhập ID nhân viên muốn xóa: ");
-        String ID = inputID.nextLine();
-
-        for (int i = 0; i < employeesListClient.size(); i++) {
-            if (employeesListClient.get(i).getEmployeeID().equals(ID)) {
-                employeesListClient.remove(i);
-            }
+    public static void deleteEmployee(){
+        Scanner input = new Scanner(System.in);
+        int choice = -1;
+        System.out.println("1. Xóa nhân viên theo mã");
+        System.out.println("2. Xóa nhân viên theo tên");
+        System.out.println("Nhập lựa chọn: ");
+        choice = input.nextInt();
+        if (choice == 1){
+            deleteEmployeeByID();
+        }
+        if (choice == 2){
+            deleteEmployeeByName();
         }
     }
 
 
 
+    public static void deleteEmployeeByID() {
+        Scanner inputID = new Scanner(System.in);
+        System.out.println(" Nhập ID nhân viên muốn xóa: ");
+        String ID = inputID.nextLine();
 
-    public static void addNewEmployee(){
+//        for (int i = 0; i < employeesListClient.size(); i++) {
+//            if (employeesListClient.get(i).getEmployeeID().equals(ID)) {
+//                employeesListClient.remove(i);
+//            }
+//        }
+
+        int deleteIndex = EmployeeManager.findEmpoyeeIndexById(ID);
+        EmployeeManager.removeEmployeeByIndex(deleteIndex);
+    }
+
+    public static void deleteEmployeeByName() {
+        Scanner inputName = new Scanner(System.in);
+        System.out.println(" Nhập tên nhân viên muốn xóa: ");
+        String name = inputName.nextLine();
+
+        int deleteIndex = EmployeeManager.findEmployeeIndexByName(name);
+        EmployeeManager.removeEmployeeByIndex(deleteIndex);
+    }
+
+
+
+//    public static void addNewEmployee(){
+//        int choice = -1;
+//        Scanner inputChoice = new Scanner(System.in);
+//        while (choice!=0){
+//            System.out.println("Add new employee:");
+//            System.out.println("1. New part time");
+//            System.out.println("2. New full time ");
+//            System.out.println("0. Return to main menu ");
+//            choice = inputChoice.nextInt();
+//            switch (choice){
+//                case 1:
+//                    Employee fulltimeEmployee = creatNewFulltimeEmployee();
+//                    EmployeeManager.addNewEmployee(fulltimeEmployee);
+//                    break;
+//                case 2:
+//                    Employee parttimeEmployee = createNewPartTimeEmployee();
+//                    EmployeeManager.addNewEmployee(parttimeEmployee);
+//                    break;
+//                case 0:
+//                    return;
+//                default:
+//                    System.out.println("please let's choose one!");
+//
+//
+//            }
+//        }
+//    }
+
+    public static Employee creatNewEmployee() {
+        Scanner input = new Scanner(System.in);
         int choice = -1;
-        Scanner inputChoice = new Scanner(System.in);
-        while (choice!=0){
-            System.out.println("Add new employee:");
-            System.out.println("1. New part time");
-            System.out.println("2. New full time ");
-            System.out.println("0. Return to main menu ");
-            choice = inputChoice.nextInt();
-            switch (choice){
-                case 1:
-                    Employee fulltimeEmployee = creatNewFulltimeEmployee();
-                    EmployeeManager.addNewEmployee(fulltimeEmployee);
-                    break;
-                case 2:
-                    Employee parttimeEmployee = createNewPartTimeEmployee();
-                    EmployeeManager.addNewEmployee(parttimeEmployee);
-                    break;
-                case 0:
-                    return;
-                default:
-                    System.out.println("please let's choose one!");
-
-
-            }
+        Employee newEmployee = null;
+        System.out.println("1. Thêm mới nhân viên Fulltime");
+        System.out.println("2. Thêm mới nhân viên Parttime");
+        System.out.println("Nhập lựa chọn: ");
+        choice = input.nextInt();
+        if (choice == 1) {
+            newEmployee = creatNewFulltimeEmployee();
         }
+        if (choice == 2) {
+            newEmployee = createNewPartTimeEmployee();
+        }
+        return newEmployee;
     }
 
     public static Employee creatNewFulltimeEmployee(){
@@ -162,6 +208,41 @@ public class Client {
 
         Employee newEmployee = new ParttimeEmployee(employeeID, fullName, age, phoneNumber, email, hour);
         return newEmployee;
+    }
+
+
+    public static void  editEmployee(){
+        Scanner input = new Scanner(System.in);
+        int choice = -1;
+        System.out.println("1. Sửa nhân viên theo id");
+        System.out.println("2. Sửa nhân viên theo tên");
+        System.out.println("Nhập lựa chọn: ");
+        choice = input.nextInt();
+        if (choice == 1){
+            editEmployeeByID();
+        }
+        if (choice == 2){
+            editEmployeeByName();
+        }
+    }
+
+    public static void editEmployeeByID(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhập ID nhân viên muốn sửa: ");
+        String editID = input.nextLine();
+        Employee editEmployee = creatNewEmployee();
+        int editIndex = EmployeeManager.findEmpoyeeIndexById(editID);
+        EmployeeManager.editEmployeeByIndex(editIndex,editEmployee);
+    }
+
+    public static void editEmployeeByName() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhập tên nguyên liệu muốn sửa: ");
+        String editName = input.nextLine();
+
+        Employee editEmployee = creatNewEmployee();
+        int editIndex = EmployeeManager.findEmployeeIndexByName(editName);
+        EmployeeManager.editEmployeeByIndex(editIndex,editEmployee);
     }
 
 
